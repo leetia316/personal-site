827b1b85a3eba7cac8b52a72c32c6d4eb6e7aa39
Matt Waler <matt.waler@thunder77-mac.local>
2019-04-29 08:27:47 -0400

    Tree-shaking dependencies.

--- plugins/globalComponents.js
+++ plugins/globalComponents.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
-import _ from 'lodash'
+import { forEach } from 'lodash'
 
 const components = require.context('~/components', false, /[A-Z]\w+\.(vue)$/)
-_.forEach(components.keys(), (fileName) => {
+forEach(components.keys(), (fileName) => {
   const componentConfig = components(fileName)
   const componentName = fileName.split('/').pop().split('.')[0]
   Vue.component(componentName, componentConfig.default || componentConfig)
 })
 
 const icons = require.context('~/assets/icons', false, /[A-Z]\w+\.(svg)$/)
-_.forEach(icons.keys(), (fileName) => {
+forEach(icons.keys(), (fileName) => {
   const iconConfig = icons(fileName)
   const iconName = fileName.split('/').pop().split('.')[0]
   Vue.component(iconName, iconConfig.default || iconConfig)
