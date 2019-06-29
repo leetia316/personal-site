ef84eb427dfb24785326e75b7ac1e0bb3d1efa6a
Matthew Waler <mattwaler@Matthews-MacBook-Pro.local>
2019-06-29 17:07:17 -0400

    Moving all config values to the top of the file!

--- tailwind.js
+++ tailwind.js
@@ -1,36 +1,43 @@
-const rem = value => `${value / 16}rem`
-
+// Font Stuff
 const fontSizes = [14, 16, 18, 20, 24, 30, 36, 48, 60, 72, 96]
-const fontSize = {}
-fontSizes.forEach((font) => {
-  fontSize[font] = rem(font)
-})
-
 const fontWeights = [100, 200, 300, 400, 500, 600, 700, 800, 900]
-const fontWeight = {}
-fontWeights.forEach((font) => {
-  fontWeight[font] = font
-})
-
 const percentSizes = [20, 25, 40, 50, 60, 75, 80, 100]
-const percentSize = {
+const percentSizesCalc = {
   '12%': 'calc(100% * (1/8))',
   '16%': 'calc(100% * (1/6))',
   '17%': 'calc(100% * (1/7))',
   '33%': 'calc(100% * (1/3))',
   '66%': 'calc(100% * (2/3))',
 }
+const remSizes = [16, 24, 32, 48, 64, 96, 128, 256, 320, 480, 640, 768, 960, 1280, 1440, 1600]
+const remSpacings = [4, 8, 12, 16, 18, 20, 24, 32, 40, 48, 56, 64, 80, 96]
+const viewportSizes = [25, 50, 75, 100]
+const screenSizes = [320, 480, 640, 768, 960, 1024, 1280, 1366]
+
+const rem = value => `${value / 16}rem`
+
+const fontSize = {}
+fontSizes.forEach((font) => {
+  fontSize[font] = rem(font)
+})
+
+const fontWeight = {}
+fontWeights.forEach((font) => {
+  fontWeight[font] = font
+})
+
+const percentSize = {
+  ...percentSizesCalc,
+}
 percentSizes.forEach((percent) => {
   percentSize[`${percent}%`] = `${percent}%`
 })
 
-const remSizes = [16, 24, 32, 48, 64, 96, 128, 256, 320, 480, 640, 768, 960, 1280, 1440, 1600]
 const remSize = {}
 remSizes.forEach((size) => {
   remSize[size] = rem(size)
 })
 
-const remSpacings = [4, 8, 12, 16, 18, 20, 24, 32, 40, 48, 56, 64, 80, 96]
 const remSpacing = {}
 remSpacings.forEach((spacing) => {
   remSpacing[spacing] = rem(spacing)
@@ -42,13 +49,11 @@ remSpacingsNegatives.forEach((spacing) => {
   remSpacingNegative[spacing] = rem(spacing)
 })
 
-const screenSizes = [320, 480, 640, 768, 960, 1024, 1280, 1366]
 const screens = {}
 screenSizes.forEach((screen) => {
   screens[`${screen}`] = `${screen}px`
 })
 
-const viewportSizes = [25, 50, 75, 100]
 const viewportSize = {}
 viewportSizes.forEach((size) => {
   viewportSize[`${size}vw`] = `${size}vw`
