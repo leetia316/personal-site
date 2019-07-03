1e19f068ccde11558f079883fb535d85b27f2261
Matt Waler <matt.waler@thunder77-mac.local>
2019-07-03 11:12:40 -0400

    More purge fixes.

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,7 +3,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
     './**/*.html',
     './**/*.vue',
   ],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/%]+/g) || []
 })
 const MODE = process.env.NODE_ENV === 'production'
 
