44461e5065e309a7198e9a25c3c215085e6aaf9d
Matthew Waler <mattwaler@Matthews-MacBook-Pro.local>
2019-07-21 17:03:02 -0400

    Building hobbies section and removing external API for now.

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,10 @@
       <div class="w-100% 768:w-66% 768:pr-32">
         <h1
           class="text-36 768:text-48 leading-none pb-8 pt-24 768:pt-0"
-          v-html="content.hero.headline" />
+          v-html="hero.headline" />
         <p
           class="pb-32"
-          v-html="content.hero.subheadline" />
+          v-html="hero.subheadline" />
         <TheButton
           newtab
           link="/MattWaler.pdf"
@@ -23,22 +23,24 @@
       <div class="w-100% 768:w-33% 768:pl-32">
         <img
           class="w-66% mx-auto rounded-lg shadow-lg 768:w-100%"
-          src="~/assets/sisters.jpg"
+          :src="hero.image"
           alt="My sisters, Hannah and Emily.">
       </div>
     </div>
 
-    <!-- Waves/Features Section -->
+    <!-- Waves -->
     <div class="w-100% relative">
       <Waves class="w-100%" />
       <div class="w-100% absolute bottom-0 bg-deep-blue h-2" />
     </div>
-    <div class="bg-deep-blue w-100% text-white">
+
+    <!-- Skills -->
+    <div class="bg-deep-blue w-100% text-white pb-32 960:pb-48">
       <div class="max-w-1280 mx-auto py-36 px-24 flex flex-wrap">
         <article
-          v-for="(item, index) in content.skills"
+          v-for="(item, index) in skills"
           :key="index"
-          class="w-100% 960:w-33% p-24 flex">
+          class="w-100% py-24 flex 960:px-24 960:w-33%">
           <component
             :is="item.icon"
             class="text-sea-green fill-current w-32 h-32 flex-none" />
@@ -51,41 +53,65 @@
         </article>
       </div>
     </div>
+
+    <!-- Hobbies Section -->
+    <div class="w-100% px-24 py-48 960:py-96">
+      <div class="max-w-1280 mx-auto text-center">
+        <h2 class="leading-none text-36 960:text-48">
+          "Is that <b><i>all</i></b> you do?"
+        </h2>
+        <p class="text-18 960:pb-96">
+          Funny you should ask, I was just about to tell you.
+        </p>
+        <div class="flex flex-wrap 960:-mx-16">
+          <article
+            v-for="(item, index) in hobbies"
+            :key="index"
+            class="w-100% pt-96 960:w-33% 960:pt-0 960:px-16">
+            <component
+              :is="item.icon"
+              class="960:h-128 1280:h-175 w-75% 960:w-auto m-auto" />
+            <p class="pt-48 text-18">
+              {{ item.text }}
+            </p>
+          </article>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
-  import axios from 'axios'
+  import { value } from 'vue-function-api'
   import TheButton from '~/components/TheButton.vue'
-
-  const { API } = process.env
-  const query = `
-    query {
-      homepageBy(id: "aG9tZXBhZ2U6NDU=") {
-        homepageACFs {
-          hero {
-            headline,
-            subheadline
-          },
-          skills {
-            headline,
-            icon,
-            paragraph
-          }
-        }
-      }
-    }`
 
   export default {
     name: 'Index',
     components: { TheButton },
-    async asyncData() {
-      const { data } = await axios.post(API, { query })
-      const content = data.data.homepageBy.homepageACFs
-      return { content }
-    },
     head: () => ({
       title: 'Matt Waler | Frontend Developer',
     }),
+    setup() {
+      const hero = value({
+        headline: 'I\'m a <b>pragmatic web developer</b> who wears many hats.',
+        subheadline: '... metaphorically speaking. I look <b><i>REALLY</b></i> bad in hats.',
+        image: '/images/sisters.jpg',
+      })
+      const skills = value([
+        { icon: 'Frontend', headline: 'Frontend Development', paragraph: 'I can turn any design into a web app. I prefer Vue.js and PostCSS, but know my way around Node, ES6, and plenty of other modern frontend tooling.' },
+        { icon: 'Backend', headline: 'Backend Development', paragraph: 'I use modern backend solutions like Headless CMSs and Static Site Generation to deploy lean and secure builds to Global CDNs.' },
+        { icon: 'Design', headline: 'Design & UX', paragraph: 'While it is not my primary focus, I can whip up low-fidelity wireframes, and iterate them into full-fledged designs. I designed this very site!' },
+      ])
+      const hobbies = value([
+        { icon: 'Exercise', text: 'I repeatedly pick up and then lower down heavy objects to make my muscles slightly bigger.' },
+        { icon: 'Party', text: 'I have been known to drink a brew or two, but I am far past my Ohio University glory days.' },
+        { icon: 'Gaming', text: 'I have been playing competitive video games for as long as I can remember. I am currently hooked on Apex Legends.' },
+      ])
+      return {
+        hero,
+        skills,
+        hobbies,
+      }
+    },
   }
 </script>
