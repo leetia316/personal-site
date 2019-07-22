b46f1374874cef53c34fd0585569abb71222015d
Matthew Waler <mattwaler@Matthews-MacBook-Pro.local>
2019-07-22 19:07:04 -0400

    Testing content files in ie11

--- content/index.js
+++ content/index.js
@@ -5,21 +5,13 @@ export const hero = {
 }
 
 export const skills = [
-  { icon: 'Frontend', headline: 'Frontend Development', paragraph: 'I can turn any design into a web app. I prefer Vue.js and PostCSS, but know my way around Node, ES6, and plenty of other modern frontend tooling.' },
-  { icon: 'Backend', headline: 'Backend Development', paragraph: 'I use modern backend solutions like Headless CMSs and Static Site Generation to deploy lean and secure builds to Global CDNs.' },
-  { icon: 'Design', headline: 'Design & UX', paragraph: 'While it is not my primary focus, I can whip up low-fidelity wireframes, and iterate them into full-fledged designs. I designed this very site!' },
+  { icon: 'Frontend', headline: 'Frontend Development', paragraph: 'I can turn any design into a web app. I prefer <b>Vue.js</b> and <b>PostCSS</b>, but know my way around <b>Node</b>, <b>Webpack</b>, and plenty of other modern frontend tooling.' },
+  { icon: 'Backend', headline: 'Backend Development', paragraph: 'I use modern backend solutions like <b>Headless CMSs</b> and <b>Static Site Generation</b> to deploy lean and secure builds to Global CDNs.' },
+  { icon: 'Design', headline: 'Design & UX', paragraph: 'While it is not my primary focus, I can whip up low-fidelity <b>wireframes</b>, and iterate them into full-fledged <b>designs</b>. I designed this very site!' },
 ]
 
 export const hobbies = [
-  { icon: 'Exercise', text: 'I repeatedly pick up and then lower down heavy objects to make my muscles slightly bigger.' },
-  { icon: 'Party', text: 'I have been known to drink a brew or two, but I am far past my Ohio University glory days.' },
-  { icon: 'Gaming', text: 'I have been playing competitive video games for as long as I can remember. I am currently hooked on Apex Legends.' },
+  { icon: 'Exercise', text: 'I repeatedly pick up and put down heavy objects to make my muscles <b>slightly</b> bigger.' },
+  { icon: 'Party', text: 'I have been known to drink a brew or two, but I am far past my <b>Ohio University</b> glory days.' },
+  { icon: 'Gaming', text: 'I have been a competitive gamer since birth. I am currently hooked on <b>Apex Legends</b>.' },
 ]
-
-const content = {
-  ...hero,
-  ...skills,
-  ...hobbies,
-}
-
-export default content
--- pages/index.vue
+++ pages/index.vue
@@ -94,21 +94,6 @@
       title: 'Matt Waler | Frontend Developer',
     }),
     setup() {
-      const hero = value({
-        headline: 'I\'m a <b>pragmatic web developer</b> who wears many hats.',
-        subheadline: '... metaphorically speaking. I look <b><i>REALLY</b></i> bad in hats.',
-        image: '/images/sisters.jpg',
-      })
-      const skills = value([
-        { icon: 'Frontend', headline: 'Frontend Development', paragraph: 'I can turn any design into a web app. I prefer <b>Vue.js</b> and <b>PostCSS</b>, but know my way around <b>Node</b>, <b>Webpack</b>, and plenty of other modern frontend tooling.' },
-        { icon: 'Backend', headline: 'Backend Development', paragraph: 'I use modern backend solutions like <b>Headless CMSs</b> and <b>Static Site Generation</b> to deploy lean and secure builds to Global CDNs.' },
-        { icon: 'Design', headline: 'Design & UX', paragraph: 'While it is not my primary focus, I can whip up low-fidelity <b>wireframes</b>, and iterate them into full-fledged <b>designs</b>. I designed this very site!' },
-      ])
-      const hobbies = value([
-        { icon: 'Exercise', text: 'I repeatedly pick up and put down heavy objects to make my muscles <b>slightly</b> bigger.' },
-        { icon: 'Party', text: 'I have been known to drink a brew or two, but I am far past my <b>Ohio University</b> glory days.' },
-        { icon: 'Gaming', text: 'I have been a competitive gamer since birth. I am currently hooked on <b>Apex Legends</b>.' },
-      ])
       return {
         hero, skills, hobbies,
       }
