39e267a4c92256ebbf9a9bcb0f55eae3b70863bc
Matt Waler <matt.waler@thunder77-mac.local>
2019-07-22 17:10:47 -0400

    Editing homepage to be more IE11 compatible.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="flex flex-col">
+  <main>
     <!-- Hero Section -->
     <div class="flex flex-wrap-reverse items-center flex-grow max-w-1080 mx-auto py-48 px-24 768:py-96">
       <div class="w-100% 768:w-66% 768:pr-32">
@@ -14,8 +14,8 @@
           link="/MattWaler.pdf"
           text="View my Resume" />
         <div class="flex items-center pt-64">
-          <Checked class="w-32 text-sea-green fill-current" />
-          <p class="uppercase text-20 text-gray-600 pl-12">
+          <Checked class="w-32 h-32 flex-shrink-0 text-sea-green fill-current" />
+          <p class="uppercase leading-snug text-20 text-gray-600 pl-12">
             Currently available for freelance & consulting
           </p>
         </div>
@@ -29,18 +29,20 @@
     </div>
 
     <!-- Waves -->
-    <div class="w-100% relative">
-      <Waves class="w-100%" />
+    <div
+      class="w-100% h-0 relative"
+      style="padding-bottom: calc(100% * 83/1366);">
+      <Waves class="absolute w-100% h-100% top-0 left-0" />
       <div class="w-100% absolute bottom-0 bg-deep-blue h-2" />
     </div>
 
     <!-- Skills -->
-    <div class="bg-deep-blue w-100% text-white pb-32 960:pb-48">
-      <div class="max-w-1280 mx-auto py-36 px-24 flex flex-wrap">
+    <div class="bg-deep-blue w-100% text-white pb-24 pt-16 960:pb-48">
+      <div class="max-w-1280 mx-auto py-36 px-16 flex flex-wrap 960-mx-16">
         <article
           v-for="(item, index) in skills"
           :key="index"
-          class="w-100% py-24 flex 960:px-24 960:w-33%">
+          class="w-100% py-24 flex 960:px-16 960:w-33%">
           <component
             :is="item.icon"
             class="text-sea-green fill-current w-32 h-32 flex-none" />
@@ -71,9 +73,9 @@
             <component
               :is="item.icon"
               class="960:h-128 1280:h-175 w-75% 960:w-auto m-auto" />
-            <p class="pt-48 text-18">
-              {{ item.text }}
-            </p>
+            <p
+              class="pt-48 text-18"
+              v-html="item.text" />
           </article>
         </div>
       </div>
@@ -98,14 +100,14 @@
         image: '/images/sisters.jpg',
       })
       const skills = value([
-        { icon: 'Frontend', headline: 'Frontend Development', paragraph: 'I can turn any design into a web app. I prefer Vue.js and PostCSS, but know my way around Node, ES6, and plenty of other modern frontend tooling.' },
-        { icon: 'Backend', headline: 'Backend Development', paragraph: 'I use modern backend solutions like Headless CMSs and Static Site Generation to deploy lean and secure builds to Global CDNs.' },
-        { icon: 'Design', headline: 'Design & UX', paragraph: 'While it is not my primary focus, I can whip up low-fidelity wireframes, and iterate them into full-fledged designs. I designed this very site!' },
+        { icon: 'Frontend', headline: 'Frontend Development', paragraph: 'I can turn any design into a web app. I prefer <b>Vue.js</b> and <b>PostCSS</b>, but know my way around <b>Node</b>, <b>Webpack</b>, and plenty of other modern frontend tooling.' },
+        { icon: 'Backend', headline: 'Backend Development', paragraph: 'I use modern backend solutions like <b>Headless CMSs</b> and <b>Static Site Generation</b> to deploy lean and secure builds to Global CDNs.' },
+        { icon: 'Design', headline: 'Design & UX', paragraph: 'While it is not my primary focus, I can whip up low-fidelity <b>wireframes</b>, and iterate them into full-fledged <b>designs</b>. I designed this very site!' },
       ])
       const hobbies = value([
-        { icon: 'Exercise', text: 'I repeatedly pick up and then lower down heavy objects to make my muscles slightly bigger.' },
-        { icon: 'Party', text: 'I have been known to drink a brew or two, but I am far past my Ohio University glory days.' },
-        { icon: 'Gaming', text: 'I have been playing competitive video games for as long as I can remember. I am currently hooked on Apex Legends.' },
+        { icon: 'Exercise', text: 'I repeatedly pick up and put down heavy objects to make my muscles <b>slightly</b> bigger.' },
+        { icon: 'Party', text: 'I have been known to drink a brew or two, but I am far past my <b>Ohio University</b> glory days.' },
+        { icon: 'Gaming', text: 'I have been a competitive gamer since birth. I am currently hooked on <b>Apex Legends</b>.' },
       ])
       return {
         hero,
